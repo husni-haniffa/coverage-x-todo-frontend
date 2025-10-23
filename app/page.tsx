4855aba50bc444faf110d7d309e78561719e1723
Husni <husniwfayo@gmail.com>
2025-10-23 23:15:24 +0530

    font updated to poppins and main page minor ui change

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
+"use client"
 import AddTasks from "@/components/AddTasks";
 import CreatedTask from "@/components/CreatedTask";
 
-
 export default function Home() {
   return (
-    <main className="container mx-auto px-4 py-12">
-      <div className="space-y-20">
+    <main className="container mx-auto px-4 py-8 md:py-12 lg:py-16">
+      <div className="space-y-8 md:space-y-12 lg:space-y-16">
           <AddTasks/>
-          <div className="space-y-10">
+          <div className="space-y-4 md:space-y-6">
              <CreatedTask/>
           </div>
       </div>
